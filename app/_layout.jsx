c1a929dd33029ddc0944cff36afd2aec0c537c37
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2024-06-21 00:22:23 +0600

    AddFeat: Welcome screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -37,7 +37,7 @@ const RootLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,59 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButtons";
+import { images } from "../constants";
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-3xl font-pblack">Index page</Text>
-      <Link href="/home">Go to home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center min-h-screen px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
