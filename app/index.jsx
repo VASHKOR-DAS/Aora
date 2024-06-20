681ddb77fef1f627d411a18c0ff8bb5be57f0395
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2024-06-20 23:14:08 +0600

    AddFeat: (tabs) Layout created

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-red-500">
       <Text className="text-3xl font-pblack">Index page</Text>
-      <Link href="/profile">Go to profile</Link>
+      <Link href="/home">Go to home</Link>
     </View>
   );
 }
