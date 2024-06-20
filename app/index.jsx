d05240d575df93bedb327d6bb4417dfabf98b5e7
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2024-06-20 20:48:30 +0600

    assets & constants setup with tailwind

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-3xl">Index page</Text>
+      <Text className="text-3xl font-pblack">Index page</Text>
       <Link href="/profile">Go to profile</Link>
     </View>
   );
