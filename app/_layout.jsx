0fc8a7e55f16e0a2006cdf00da452ba22afcf060
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2024-06-20 20:11:10 +0600

    Native-wind added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import "../constants/styles.css";
 
 export default function RootLayout() {
   return (
---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-yellow-500">
+      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
